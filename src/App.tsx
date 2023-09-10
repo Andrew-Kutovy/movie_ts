10db1aa61b3449eaf9efba76ad3f05daef2e15fe
home <andrijkutovij5@gmail.com>
2023-09-10 11:51:00 +0300

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const changeCount = (type:string):number => {
+    setCount(type === 'increment'? count+1: count-1)
+    return count
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      count: {count}
+      <button onClick={()=>changeCount('increment')}>+1</button>
+      <button onClick={()=>changeCount('decrement')}>-1</button>
     </div>
   );
 }
